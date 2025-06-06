d3f2b03d1d3a39950e0a6dcaee240d987cd9e70f
weizhen94 <weizhen94@gmail.com>
2025-06-07 01:04:23 +0800

    PlusMinus with question

--- src/codes/PlusMinus.java
+++ src/codes/PlusMinus.java
@@ -17,6 +17,31 @@ class Result {
     /*
      * Complete the 'plusMinus' function below.
      *
+     * Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal.
+     * Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to 10^-4 are acceptable.
+Example
+
+There are 5 elements, two positive, two negative and one zero. Their ratios are 2/5 , 2/5 and 1/5 . Results are printed as:
+0.400000
+0.400000
+0.200000
+Function Description
+Complete the plusMinus function in the editor below.
+plusMinus has the following parameter(s):
+int arr[n]: an array of integers
+Print
+Print the ratios of positive, negative and zero values in the array. Each value should be printed on a separate line with  digits after the decimal. The function should not return a value.
+Input Format
+The first line contains an integer, , the size of the array.
+The second line contains  space-separated integers that describe .
+Constraints
+
+
+Output Format
+Print the following  lines, each to  decimals:
+proportion of positive values
+proportion of negative values
+proportion of zeros
      * The function accepts INTEGER_ARRAY arr as parameter.
      */
 
