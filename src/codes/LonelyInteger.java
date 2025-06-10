eb459709862b87b604d108c5f80bcf6b2c5cf7a3
weizhen94 <weizhen94@gmail.com>
2025-06-10 22:47:05 +0800

    LonelyInteger new

--- src/codes/LonelyInteger.java
+++ src/codes/LonelyInteger.java
@@ -24,30 +24,51 @@ class Result {
     public static int lonelyinteger(List<Integer> a) {
     // Write your code here
     //System.out.println(a); 
+
+    a.sort(null); 
     
     int currentNumber = 0;  
     
-    for(int i = 0; i < a.size(); i++){
+    for(int i = 0; i < a.size() - 1; i++){
         
         //currentNumber = a.get(i); 
-        
-        for(int j = 0; j < a.size(); j++){
             
-            if(i != j && a.get(i) == a.get(j)){
-                
-                //System.out.println("i is: " + i + ". j is: " + j + ". a.get(i) is: " + a.get(i) + ". a.get(j) is: " + a.get(j)); 
-                
-                System.out.println(a.get(j)); 
+            if(a.get(i) != a.get(i+1)){
 
-                a.remove(j);
+              if(i == 0){
+                System.out.println("inside (i == 0): a.get(i) = " + a.get(i) + ". a.get(i+1) = " + a.get(i+1)); 
+                currentNumber = a.get(i);
+                System.out.println("Current number is: " + currentNumber); 
+              }
+
+              if(i + 2 < a.size() && a.get(i+1) != a.get(i+2)){
+                System.out.println("inside (a.get(i+1) != a.get(i+2): a.get(i) = " + a.get(i) + ". a.get(i+1) = " + a.get(i+1) + ". a.get(i+2) = " + a.get(i+2)); 
+                currentNumber = a.get(i+1);
+                System.out.println("Current number is: " + currentNumber); 
+              }
+
+              if(i == a.size() - 1){
+                System.out.println("inside (i == a.size() - 1): a.get(i) = " + a.get(i) + ". a.get(i+1) = " + a.get(i+1)); 
+                currentNumber = a.get(i+1);
+                System.out.println("Current number is: " + currentNumber); 
+              }
                 
-            } else {
-                currentNumber = a.get(i); 
-            }
+                //System.out.println("i is: " + i + ".  is: " + j + ". a.get(i) is: " + a.get(i) + ". a.get(j) is: " + a.get(j)); 
+                
+                //System.out.println(a.get(j)); 
+
+                //a.remove(j);
+                
+            } 
+            // else {
+            //     currentNumber = a.get(i); 
+            //     System.out.println("Current number is: " + currentNumber); 
+            // }
             
-        }
+        
         
     }
+    //System.out.println("Current number is: " + currentNumber); 
     return currentNumber; 
     }
 
@@ -55,21 +76,29 @@ class Result {
 
 public class LonelyInteger {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        // BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        // int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-            .map(Integer::parseInt)
-            .collect(toList());
+        List<Integer> a = new LinkedList<>();
+        a.add(1);  
+        a.add(2);  
+        a.add(4);  
+        a.add(3);  
+        a.add(3);  
+        a.add(2);  
+        a.add(1);  
+        // Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+        //     .map(Integer::parseInt)
+        //     .collect(toList());
 
         int result = Result.lonelyinteger(a);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
+        // bufferedWriter.write(String.valueOf(result));
+        // bufferedWriter.newLine();
 
-        bufferedReader.close();
-        bufferedWriter.close();
+        // bufferedReader.close();
+        // bufferedWriter.close();
     }
 }
