53c3f68c388d30ae007e63183cbfbf9a1ae10d79
weizhen94 <weizhen94@gmail.com>
2025-06-14 23:55:02 +0800

    new just

--- src/codes/New.java
+++ src/codes/New.java
@@ -4,6 +4,11 @@ public class New {
 
     public static void main(String[] args) {
         System.out.println("Just to commit");
+System.out.println("Just to commit");
+System.out.println("Just to commit");
+System.out.println("Just to commit");
+System.out.println("Just to commit");
+
     }
     
 }
