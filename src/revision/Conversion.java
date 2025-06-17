55961d7190eab1bb0d899048808dc2535dd4b384
weizhen94 <weizhen94@gmail.com>
2025-06-17 22:08:18 +0800

    substring

--- src/revision/Conversion.java
+++ src/revision/Conversion.java
@@ -4,6 +4,12 @@ public class Conversion {
 
     public static void main(String args[]){
 
+        String numberString = "3";
+        System.out.println("This is string: " + numberString);  
+        int numberInt = Integer.parseInt(numberString); // this is the way to convert string to int
+        System.out.println("This is int: " + numberInt); 
+
+        // this is the way to convert int, double, long, float, char, boolean to string 
         int number = 4; 
         String numberInString = String.valueOf(number); 
         System.out.println("This is numberInString: " + numberInString); 
@@ -27,6 +33,28 @@ public class Conversion {
         boolean b = true; 
         String booleanInString = String.valueOf(b); 
         System.out.println("This is booleanInString: " + booleanInString); 
+
+        int newNumber = 2147483647;
+        long newNumberLong = (long) newNumber * 2; 
+        System.out.println("This is newNumberLong: " + newNumberLong); 
+        long newNumberNoLong = newNumber * 2; 
+        System.out.println("This is newNumberNoLong: " + newNumberNoLong); 
+
+        //showing decimal points
+        double positive = 5; 
+        int divide = 3; 
+        // when you take a double divide by int the final result will follow the double
+        String decimalRatio = String.format("%.6f" , positive / divide); //this gives 6 decimal places
+        System.out.println("This is decimalRatio: " + decimalRatio); 
+
+
+        //to do substring
+        String s = "07:05:45PM"; 
+        String time = s.substring(8, 10); // s.substring(8, 10) same as s.substring(8) - 8 is beginIndex, 10 is endIndex
+        System.out.println("This is timeInt: " + time); 
+        if(s.substring(8).equals("PM")){ //for string cannot ==, need to do .equals
+            System.out.println("This is .equals output " + s.substring(8)); 
+        }
     }
     
 }
