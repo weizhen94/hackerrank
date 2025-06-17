731bc7c5abcfb419c15a4128ba6f5704162ae541
weizhen94 <weizhen94@gmail.com>
2025-06-17 21:01:34 +0800

    conversion

--- src/revision/Conversion.java
+++ src/revision/Conversion.java
@@ -4,11 +4,29 @@ public class Conversion {
 
     public static void main(String args[]){
 
-        String numberString = "3";
-        System.out.println("This is string: " + numberString);  
-        int numberInt = Integer.parseInt(numberString); // this is the way to convert string to int
-        System.out.println("This is int: " + numberInt); 
+        int number = 4; 
+        String numberInString = String.valueOf(number); 
+        System.out.println("This is numberInString: " + numberInString); 
 
+        double doubleValue = 3.1415926;
+        String doubleInString = String.valueOf(doubleValue);
+        System.out.println("This is doubleInString: " + doubleInString); 
+
+        float floatValue = 3.1415926f;
+        String floatInString = String.valueOf(floatValue);
+        System.out.println("This is floatInString: " + floatInString); 
+
+        long longValue = 10000000000L;
+        String longInString = String.valueOf(longValue); 
+        System.out.println("This is longInString: " + longInString); 
+
+        char chinese = '你';
+        String charInString = String.valueOf(chinese);
+        System.out.println("This is charInString: " + charInString); 
+
+        boolean b = true; 
+        String booleanInString = String.valueOf(b); 
+        System.out.println("This is booleanInString: " + booleanInString); 
     }
     
 }
