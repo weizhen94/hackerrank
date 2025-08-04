0b98c03af5ae6746d57fb3863ab5e18a4ae95de3
weizhen94 <weizhen94@gmail.com>
2025-08-04 23:20:52 +0800

    04082025 2320

--- src/codes/Word.java
+++ src/codes/Word.java
@@ -1,36 +1,61 @@
 package codes;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Word {
 
     public String isValidWordGrid(List<String> words){
-        words.size(); 
+        //words.size(); 
         int i = 0;
-        //char letters = ' ';  
         char[] letters = new char[words.size()]; 
+        List<String> oneWord = new LinkedList<>();
 
         while(i < words.size()){
+            int j = 0;
             for(String wordString : words){
-            letters[i] = wordString.charAt(i); 
+            System.out.println("wordString is: " + wordString); 
+            letters[j] = wordString.charAt(i); 
+            j++;
+            System.out.println("letters is: " + Arrays.toString(letters)); 
             }
-            List<String> oneWord = new LinkedList<>();
             oneWord.add(new String(letters));
-            if(oneWord.add(new String(letters)).equals(words.get))
-            i++; 
+            System.out.println("oneWord is: " + oneWord); 
+            // Method 1: !words.contains(oneWord.get(i))
+            // System.out.println("!words.contains(oneWord.get(i)) is : " + !words.contains(oneWord.get(i)));
+            // if(!words.contains(oneWord.get(i))){
+            //     return "NO"; 
+            // } 
+
+            // i++; 
+
+            // Method 2: words.contains(oneWord.get(i))
+            System.out.println("words.contains(oneWord.get(i)) is : " + words.contains(oneWord.get(i)));
+            if(words.contains(oneWord.get(i))){
+                i++;
+            } else {
+                return "NO"; 
+            }
         }
 
-        return ""; 
+        return "YES"; 
     }
     
     public static void main(String args[]){
 
         List<String> words = new LinkedList<>(); 
-        words.add("ball"); 
-        words.add("area"); 
-        words.add("lead"); 
-        words.add("lady"); 
+        // YES
+        // words.add("ball"); 
+        // words.add("area"); 
+        // words.add("lead"); 
+        // words.add("lady"); 
+
+        //NO
+        words.add("cool"); 
+        words.add("deal"); 
+        words.add("lamp");
+        words.add("mood"); 
 
         Word word = new Word(); 
         String validString = word.isValidWordGrid(words); 
