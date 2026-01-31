ac679a32c4fc65392e4c0a5c366507280e8fe984
Wei Zhen Gong <wgong40@gatech.edu>
2026-01-31 22:33:23 +0800

    31/01/2026 10:33pm

--- src/blind75/ValidParentheses.java
+++ src/blind75/ValidParentheses.java
@@ -35,4 +35,16 @@ public class ValidParentheses {
         
     }
 
+    public boolean checkBracketForward(int i, String s, char c){
+
+        if(i+1 < s.length() && s.charAt(i+1) == c) return true; 
+
+        if(!s.contains(String.valueOf(c))) return false; 
+
+        if(i < s.length()-1-i && s.charAt(s.length()-1-i) == c) return true; 
+
+        return false; 
+
+    }
+
 }
